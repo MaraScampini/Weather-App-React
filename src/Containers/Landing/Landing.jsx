53f99434cbb0e36a0fbb849f479e504dade5738a
MaraScampini <109754827+MaraScampini@users.noreply.github.com>
2022-12-27 12:02:46 +0100

    [Feat] Change navigation to main buttons in landing page and add styling

--- src/Containers/Landing/Landing.jsx
+++ src/Containers/Landing/Landing.jsx
@@ -1,16 +1,34 @@
 import React, { useContext } from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { RegionContext } from "../../Context/RegionContext";
 
 function Landing() {
   const {regionHandler} = useContext(RegionContext);
-  const navigate=useNavigate();
+    const navigate = useNavigate();
+
+  const navigationHandler = (region) => {
+    regionHandler(region);
+    navigate("/dashboard");
+  }
   return (
-    <div>
-      <button onClick={() => regionHandler("europe")}>Europe</button>
-      <button onClick={() => regionHandler("northamerica")}>North America</button>
-      <button onClick={() => navigate("/dashboard")}>Go</button>
-    </div>
+    <Container className="d-flex flex-column justify-content-center">
+      <Row>
+        <Col>
+          <span className='titleSpan'>CHOOSE YOUR REGION</span>
+        </Col>
+      </Row>
+      <Row className="d-flex justify-content-center">
+        <Col md="4" className="d-flex flex-column">
+          <button onClick={() => navigationHandler("europe")} id="cityButton">
+            Europe
+          </button>
+          <button onClick={() => navigationHandler("northamerica")} id="cityButton">
+            North America
+          </button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 

2b20bd18d55652ebd050c3f86c05fa07474e6369
MaraScampini <109754827+MaraScampini@users.noreply.github.com>
2022-12-27 12:26:30 +0100

    [Feat] Finished styling for landing page

--- src/Containers/Landing/Landing.jsx
+++ src/Containers/Landing/Landing.jsx
@@ -12,22 +12,27 @@ function Landing() {
     navigate("/dashboard");
   }
   return (
-    <Container className="d-flex flex-column justify-content-center">
-      <Row>
-        <Col>
-          <span className='titleSpan'>CHOOSE YOUR REGION</span>
-        </Col>
-      </Row>
-      <Row className="d-flex justify-content-center">
-        <Col md="4" className="d-flex flex-column">
-          <button onClick={() => navigationHandler("europe")} id="cityButton">
-            Europe
-          </button>
-          <button onClick={() => navigationHandler("northamerica")} id="cityButton">
-            North America
-          </button>
-        </Col>
-      </Row>
+    <Container className="d-flex flex-column justify-content-center landing">
+      {/* <div className="landing"> */}
+        <Row>
+          <Col className="d-flex justify-content-center">
+            <span className="titleSpan">CHOOSE YOUR REGION</span>
+          </Col>
+        </Row>
+        <Row className="d-flex justify-content-center">
+          <Col sm="8" md="4" lg="3" className="d-flex flex-column">
+            <button onClick={() => navigationHandler("europe")} id="cityButton">
+              Europe
+            </button>
+            <button
+              onClick={() => navigationHandler("northamerica")}
+              id="cityButton"
+            >
+              North America
+            </button>
+          </Col>
+        </Row>
+      {/* </div> */}
     </Container>
   );
 }

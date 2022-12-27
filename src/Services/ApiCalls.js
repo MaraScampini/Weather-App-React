5a639bf784d7e440aae1be5d61dd423198f1c810
MaraScampini <109754827+MaraScampini@users.noreply.github.com>
2022-12-27 15:01:09 +0100

    [Fix] Fixed apicall route

--- src/Services/ApiCalls.js
+++ src/Services/ApiCalls.js
@@ -2,19 +2,9 @@ import axios from "axios";
 
 const URL_CURRENT = "http://api.openweathermap.org/data/2.5/weather?q=";
 const URL_FORECAST = "https://api.openweathermap.org/data/2.5/forecast?q=";
-const apiKey = "&APPID=d356c60038a283707afc8ed0b68c954a";
+const apiKey = "&APPID=0ad001533b5fbb8d3d823196f6909479";
 const units = "&units=metric";
 
-// Example call = ${URL}London,uk${apiKey};
-
-export const Current = async (city) => {
-  try {
-    let res = await axios.get(`${URL_CURRENT}${city}${apiKey}${units}`);
-    return res.data;
-  } catch (error) {
-    console.error(error);
-  }
-};
 
 // https://api.openweathermap.org/data/2.5/forecast?q=london,uk&appid=d356c60038a283707afc8ed0b68c954a
 

6e20ab586b1deed75de8b9518f2d0c0fce903234
MaraScampini <109754827+MaraScampini@users.noreply.github.com>
2022-12-27 16:13:50 +0100

    [Chore] Add commentaries to code

--- src/Context/RegionContext.jsx
+++ src/Context/RegionContext.jsx
@@ -3,12 +3,13 @@ import React, { createContext, useEffect, useState } from "react";
 export const RegionContext = createContext();
 
 export function RegionContextProvider({ children }) {
+  // Sets a general state for region that will be used by the landing and dashboard containers and their children components.
   const [region, setRegion] = useState("");
+  // The function to set the region that will be used by the landing page's buttons also sets said region into the localStorage so the data isn't lost when the user recharges the page.
   const regionHandler = (region) => {
     setRegion(region);
     localStorage.setItem("region", region);
   };
-
 
   return (
     <RegionContext.Provider
